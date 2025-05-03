fa3c386519516e26d9d9b6664837050f8ee68f34
sumayatabassum161 <sumayatabassum161@gmail.com>
2025-05-03 20:58:33 +0600

    Day 3 class
    practice

---
+++ 1st R.R
@@ -0,0 +1,3 @@
+data=c(7, 9, 3, 40, 1, 0)
+s=mean(data)
+x=s/2
---
+++ RDSML-8-Day-3/r.R
@@ -0,0 +1,8 @@
+# My informations
+my_age = 31
+my_weight = 65
+my_height = 70
+x = (my_height*my_age)/my_weight
+print(x)
+students_height=c(40, 90, 57, 75)
+mean(students_height)
