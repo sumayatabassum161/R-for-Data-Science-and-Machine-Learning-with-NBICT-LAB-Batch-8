f1a2b08ee78cbcd91fa7155d00efd22179f1ca71
sumayatabassum161 <sumayatabassum161@gmail.com>
2025-05-07 22:50:46 +0600

    Day-2 Practice
    Operators

---
+++ RDSML-8-Day-4/rBasics.day 2.R
@@ -0,0 +1,35 @@
+#Data Types in R
+# 1.Numeric-Integer/Floating Point
+# 2. String/Character-'Sumaya Tabassum"
+# 3. Logical
+a=11
+b=7.19
+class(a)
+class(b)
+name='Sumaya'
+class(name)
+print(name)
+c="161"
+print(c+9)
+print(a+9)
+class(c)
+#Logical Data Type- TRUE,FALSE(T/F)
+class(a>b)
+print(a<b)
+x=19
+y=22-3
+print(x==y)
+print(x!=y)
+print(y>=x)
+# Logical operator in R
+4==4&&5==5
+# &&logical operator
+# || logical operator 
+x!=y || x==y
+# Simple mathematical operator in R
+z=6+9-8*2/3
+print(z)
+y=3^2
+print(y)
+11/5
+11%%5
